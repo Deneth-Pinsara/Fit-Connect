f4bcb50aaa417be39dd98a96bfba04beeda8830c
Nimsara Mahagedara <nimsaramahagedara@gmail.com>
2025-03-27 00:08:41 +0530

    topnav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import { AuthProvider } from './context/AuthContext'
 import Overview from './pages/Overview'
 import Community from './pages/Community'
+import Faq from './pages/Faq'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Overview />} />
           <Route path="/community" element={<Community />} />
+          <Route path="/faq" element={<Faq />} />
 
         </Routes>
       </BrowserRouter>
---
+++ client/src/components/TopNav.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import logo from '../assets/FitConnect.png'
+
+const navItems = [
+    {
+        name: "Home",
+        link: "/"
+    },
+    {
+        name: "Challenges",
+        link: "/challenges"
+    },
+    {
+        name: "Gyms",
+        link: "/gyms"
+    },
+    {
+        name: "Q&A",
+        link: "/faq"
+    }
+]
+const TopNav = () => {
+    return (
+        <div className='bg-black p-3 flex items-center justify-between'>
+            <div className='w-32'>
+                <img src={logo} alt="logo" />
+            </div>
+            <div className='float-end flex w-max'>
+
+                {
+                    navItems.map((item, index) => {
+                        return (
+                            <div className='text-white px-3' key={index}>
+                                <a href={item.link}>{item.name}</a>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default TopNav
